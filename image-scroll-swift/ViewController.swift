fce166977af83504400012f15ae695c16cc85310
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-04 11:40:12 +1000

    Position image

--- image-scroll-swift/ViewController.swift
+++ image-scroll-swift/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
   @IBOutlet weak var scrollView: UIScrollView!
   @IBOutlet weak var imageView: UIImageView!
@@ -18,10 +18,32 @@ class ViewController: UIViewController {
   @IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
   @IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
 
-  var lastZoomScale: CGFloat = 1
+  var lastZoomScale: CGFloat = 0
 
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
+
+    imageView.image = UIImage(named: "wallabi.jpg")
+    scrollView.delegate = self
+    updateZoom()
+  }
+
+  // Zoom to show as much image as possible unless image is smaller than screen
+  private func updateZoom() {
+    if let image = imageView.image {
+      var minZoom = min(view.bounds.size.width / image.size.width,
+        view.bounds.size.height / image.size.height)
+
+      if minZoom > 1 { minZoom = 1 }
+
+      scrollView.minimumZoomScale = minZoom
+
+      // Force scrollViewDidZoom fire if zoom did not change
+      if minZoom == lastZoomScale { minZoom += 0.000001 }
+
+      scrollView.zoomScale = minZoom
+      lastZoomScale = minZoom
+    }
   }
 
 }
