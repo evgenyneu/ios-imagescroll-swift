95bdab971897fe019e446a5c415535f24d6f0203
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-04 12:02:38 +1000

    Make status bar text white

--- image-scroll-swift/ViewController.swift
+++ image-scroll-swift/ViewController.swift
@@ -28,6 +28,18 @@ class ViewController: UIViewController, UIScrollViewDelegate {
     updateZoom()
   }
 
+  // Update zoom scale and constraints
+  // It will also animate because willAnimateRotationToInterfaceOrientation
+  // is called from within an animation block
+  //
+  // DEPRECATION NOTICE: This method is said to be deprecated in iOS 8.0. But it still works.
+  override func willAnimateRotationToInterfaceOrientation(
+    toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+
+    super.willAnimateRotationToInterfaceOrientation(toInterfaceOrientation, duration: duration)
+    updateZoom()
+  }
+
   // Zoom to show as much image as possible unless image is smaller than screen
   private func updateZoom() {
     if let image = imageView.image {
@@ -44,6 +56,43 @@ class ViewController: UIViewController, UIScrollViewDelegate {
       scrollView.zoomScale = minZoom
       lastZoomScale = minZoom
     }
+  }
+
+  func updateConstraints() {
+    if let image = imageView.image {
+      let imageWidth = image.size.width
+      let imageHeight = image.size.height
+
+      let viewWidth = view.bounds.size.width
+      let viewHeight = view.bounds.size.height
+
+      // center image if it is smaller than screen
+      var hPadding = (viewWidth - self.scrollView.zoomScale * imageWidth) / 2
+      if hPadding < 0 { hPadding = 0 }
+
+      var vPadding = (viewHeight - self.scrollView.zoomScale * imageHeight) / 2
+      if vPadding < 0 { vPadding = 0 }
+
+      imageConstraintLeft.constant = hPadding
+      imageConstraintRight.constant = hPadding
+
+      imageConstraintTop.constant = vPadding
+      imageConstraintBottom.constant = vPadding
+
+      // Makes zoom out animation smooth and starting from the right point not from (0, 0)
+      view.layoutIfNeeded()
+    }
+  }
+
+  // UIScrollViewDelegate
+  // -----------------------
+
+  func scrollViewDidZoom(scrollView: UIScrollView) {
+    updateConstraints()
+  }
+
+  func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+    return imageView
   }
 
 }
