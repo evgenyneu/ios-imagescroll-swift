78fbaa470c4909b15abf4873e59890a90c4ef57a
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-04 11:35:06 +1000

    Layout views in storyboard

--- image-scroll-swift/ViewController.swift
+++ image-scroll-swift/ViewController.swift
@@ -10,16 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
+  @IBOutlet weak var scrollView: UIScrollView!
+  @IBOutlet weak var imageView: UIImageView!
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+  @IBOutlet weak var imageConstraintTop: NSLayoutConstraint!
+  @IBOutlet weak var imageConstraintRight: NSLayoutConstraint!
+  @IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
+  @IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
 
+  var lastZoomScale: CGFloat = 1
+
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+  }
 
 }
 
