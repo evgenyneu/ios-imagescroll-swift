009196c2644a7974b98f4d48915bae881a7991c7
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-10-04 11:14:55 +1000

    Initial Commit

---
+++ image-scroll-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  image-scroll-swift
+//
+//  Created by Evgenii Neumerzhitckii on 4/10/2014.
+//  Copyright (c) 2014 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
